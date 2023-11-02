575125dddeaedd3629f2ee5b28a177373faf16f8
fairuzulum <usleunmdi32@gmail.com>
2023-11-03 06:42:07 +0700

    02 Tipe Data Number

---
+++ 02-TipeDataNumber.php
@@ -0,0 +1,27 @@
+<?php
+
+echo "Decimal : ";
+var_dump(1234);
+
+echo "Octal : ";
+var_dump(01234);
+
+echo "Binary : ";
+var_dump(0b11001);
+
+echo "Hexadecimal : ";
+var_dump(0x1A);
+
+echo "Floating Point : ";
+var_dump(1.234);
+
+echo "Floating Point dengan E Notation : (1.7 x 1000) : ";
+var_dump(1.7e3);
+
+echo  "Floating Point dengan E Notation minus : (1.7 0.001) : ";
+var_dump(1.7e-3);
+
+
+
+
+
