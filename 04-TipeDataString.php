870509462f11b691476669fc59bd2df809a573c6
fairuzulum <usleunmdi32@gmail.com>
2023-11-07 20:49:52 +0700

    04 Tipe Data String

---
+++ 04-TipeDataString.php
@@ -0,0 +1,29 @@
+<?php
+
+// Petik Satu
+echo 'Name : ';
+echo 'Ahmad Fairuz Ulumuddin';
+echo "\n";
+
+// Petik Dua
+echo "Name : ";
+echo "Ahmad\t Fairuz\t Ulumuddin\n";
+
+// Heredoc
+echo <<<FAIRUZ
+Selamat belajar PHP 
+sekarang kita belajar tipe data string
+ini adalah cara ke-3 membuat string
+bisa menggunakan heredoc
+FAIRUZ;
+
+// Nowdoc
+echo <<<'FAIRUZ'
+Selamat belajar PHP 
+sekarang kita belajar tipe data string
+ini adalah cara ke-4 membuat string
+bisa menggunakan nowdoc
+FAIRUZ;
+
+
+
