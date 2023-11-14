5ac9089c51d264bf69031e84a4884534c39f29fd
fairuzulum <usleunmdi32@gmail.com>
2023-11-14 13:11:21 +0700

    26 Go To

---
+++ 26-GoTo.php
@@ -0,0 +1,22 @@
+<?php
+
+goto a;
+echo "Hello World";
+
+a:
+echo "Hello A" . PHP_EOL;
+
+
+$counter = 1;
+
+while (true) {
+    echo "ini adalah while loop ke-$counter" . PHP_EOL;
+    $counter++;
+
+    if ($counter > 10) {
+        goto end;
+    }
+}
+
+end:
+echo "End Loop";
