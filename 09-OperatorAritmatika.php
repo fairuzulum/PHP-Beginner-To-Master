bbc20b36ea0998688cca7c378f44c51b6ecbc373
fairuzulum <usleunmdi32@gmail.com>
2023-11-14 10:16:53 +0700

    09 Operator Aritmatika

---
+++ 09-OperatorAritmatika.php
@@ -0,0 +1,34 @@
+<?php
+
+$a = 10;
+$b = 3;
+
+// Penjumlahan
+$resultPenjumlahan = $a + $b;
+var_dump($resultPenjumlahan);
+
+// Pengurangan
+$resultPengurangan = $a - $b;
+var_dump($resultPengurangan);
+
+// Perkalian
+$resultPerkalian = $a * $b;
+var_dump($resultPerkalian);
+
+// Pembagian
+$resultPembagian = $a / $b;
+var_dump($resultPembagian);
+
+// Modulus (sisa bagi)
+$resultModulus = $a % $b;
+var_dump($resultModulus);
+
+// Perpangkatan
+$resultPangkat = $a - $b;
+var_dump($resultPangkat);
+
+// positif & negatif
+$resultPositive = -$resultPenjumlahan;
+$resultNegative = +$resultPenjumlahan;
+var_dump($resultNegative);
+var_dump($resultPositive);
